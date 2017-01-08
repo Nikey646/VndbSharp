e4fbe41b59cbd025d3ca60c1415196ba0ee7d8b6
Micah686 <legendofzeldafan3@gmail.com>
2017-01-07 21:41:58 -0800

    Added files I forgot to add previously

---
+++ VndbSharp/Structs/Models/Release/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VndbSharp.Structs.Models.Release
+{
+    public class Media
+    {
+        [JsonProperty("medium")]
+        public String Medium;
+        [JsonProperty("qty")]
+        public Int32? Quantity;
+    }
+}
---
+++ VndbSharp/Structs/Models/Release/Producers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VndbSharp.Structs.Models.Release
+{
+    public class Producers
+    {
+        [JsonProperty("id")]
+        public Int32 Id;
+        [JsonProperty("developer")]
+        public Boolean Developer;
+        [JsonProperty("publisher")]
+        public Boolean Publisher;
+        [JsonProperty("name")]
+        public String Name;
+        [JsonProperty("original")]
+        public String OriginalName;
+        [JsonProperty("type")]
+        public String ProducerType;
+    }
+}
