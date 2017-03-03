389a7e84e6ea5e4c3bd0f678183d709b8c072385
Nikey646 <nikey646@users.noreply.github.com>
2017-03-04 10:24:25 +1100

    Adding the Traits filter for #15 to the NetCore branch

--- VndbSharp/VndbFilters.cs
+++ VndbSharp/VndbFilters.cs
@@ -206,6 +206,31 @@ namespace VndbSharp
 				=> this.ValidOperators.Contains(this.Operator);
 		}
 
+		// So kindly stolen from http://vndb.org/d11
+		/// <summary>
+		///		<para>Find chars by traits. When providing an array of ints, the '=' filter will return chars that are linked to any (not all) of the given traits, the '!=' filter will return chars that are not linked to any of the given traits. You can combine multiple trait filters with 'and' and 'or' to get the exact behavior you need.</para>
+		///		<para>This filter may used cached data, it may take up to 24 hours before a char entry will have its traits updated with respect to this filter.</para>
+		///		<para>Chars that are linked to childs of the given trait are also included.</para>
+		///		<para>Be warned that this filter ignores spoiler settings, fetch the traits associated with the returned char to verify the spoiler level.</para>
+		/// </summary>
+		public class Traits : AbstractFilter<UInt32[]>
+		{
+			private Traits(UInt32[] value, FilterOperator filterOperator) : base(value, filterOperator)
+			{ }
+
+			protected override FilterOperator[] ValidOperators { get; } = {
+				FilterOperator.Equal, FilterOperator.NotEqual
+			};
+
+			protected override String FilterName { get; } = "traits";
+
+			public static Traits Equals(params UInt32[] value) => new Traits(value, FilterOperator.Equal);
+			public static Traits NotEquals(params UInt32[] value) => new Traits(value, FilterOperator.NotEqual);
+
+			public override Boolean IsFilterValid()
+				=> this.ValidOperators.Contains(this.Operator);
+		}
+
 		public class Name : AbstractFilter<String>
 		{
 			private Name(String value, FilterOperator filterOperator) : base(value, filterOperator)
