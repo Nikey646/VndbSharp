e1d599e6aaf24117496bc75c1c0b1b67067c33df
Micah686 <legendofzeldafan3@gmail.com>
2017-05-22 13:28:49 -0700

    Fixed IRequestOptions

--- VndbSharp/Interfaces/IRequestOptions.cs
+++ VndbSharp/Interfaces/IRequestOptions.cs
@@ -8,22 +8,25 @@ namespace VndbSharp.Interfaces
 	/// </summary>
 	public interface IRequestOptions
 	{
-		/// <summary>
-		///		The page to get the result from
-		/// </summary>
-		Int32? Page { get; }
-		/// <summary>
-		///		How many Results to get
-		/// </summary>
-		[JsonProperty("results")]
-		Int32? Count { get; }
-		/// <summary>
-		///		 The field to order the results by. The accepted field names differ per type, the default sort field is the ID of the database entry.
-		/// </summary> 
-		String Sort { get; }
-		/// <summary>
-		///		Sets if the order of the results be reversed or not.
-		/// </summary>
-		Boolean? Reverse { get; }
+	    /// <summary>
+	    ///		The page to get the result from
+	    /// </summary>
+	    [JsonProperty("page")]
+	    Int32? Page { get; }
+	    /// <summary>
+	    ///		How many Results to get
+	    /// </summary>
+	    [JsonProperty("results")]
+	    Int32? Count { get; }
+	    /// <summary>
+	    ///		 The field to order the results by. The accepted field names differ per type, the default sort field is the ID of the database entry.
+	    /// </summary> 
+	    [JsonProperty("sort")]
+	    String Sort { get; }
+	    /// <summary>
+	    ///		Sets if the order of the results be reversed or not.
+	    /// </summary>
+	    [JsonProperty("reverse")]
+	    Boolean? Reverse { get; }
 	}
 }
