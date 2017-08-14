55d7e42ba54e99e5e26af46e3f38c0d48548ea8b
Nikey646 <nikey646@users.noreply.github.com>
2017-08-15 06:52:29 +1000

    Added the UserId property on the User/VisualNovelList, User/VoteList, and User/Wishlist models (Breaking Change) Renamed the Id property to VisualNovelId on the User/VisualNovelList, User/VoteList, and User/Wishlist models

--- VndbSharp/Models/User/VisualNovelList.cs
+++ VndbSharp/Models/User/VisualNovelList.cs
@@ -8,7 +8,9 @@ namespace VndbSharp.Models.User
 	public class VisualNovelList
 	{
 		[JsonProperty("vn")]
-		public UInt32 Id { get; private set; }
+		public UInt32 VisualNovelId { get; private set; }
+		[JsonProperty("uid")]
+		public UInt32 UserId { get; private set; }
 		public Status Status { get; private set; }
 		[JsonProperty("added"), IsUnixTimestamp]
 		public DateTime AddedOn { get; private set; }
